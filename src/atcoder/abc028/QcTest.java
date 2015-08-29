4fd5c9bdb2ae27188fadd15036d66d0cf6c5ed77
elzzup <guild0105@gmail.com>
2015-08-29 22:44:58 +0900

    try abc028

---
+++ src/atcoder/abc028/QcTest.java
@@ -0,0 +1,11 @@
+package atcoder.abc028;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class QcTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> opts = new ArrayList<>();
+    }
+}
