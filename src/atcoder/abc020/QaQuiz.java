ca2698005d6dea0dbbfb0a93370b2a10596a327f
elzzup <guild0105@gmail.com>
2016-01-24 14:33:16 +0900

    WallCost

---
+++ src/atcoder/abc020/QaQuiz.java
@@ -0,0 +1,11 @@
+package atcoder.abc020;
+
+import java.util.Scanner;
+
+public class QaQuiz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(new String[]{"", "ABC", "chokudai"}[n]);
+    }
+}
