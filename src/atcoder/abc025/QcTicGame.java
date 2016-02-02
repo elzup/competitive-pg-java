143ddb712c7a139d8dfcf694de1252e30f084eb4
elzzup <guild0105@gmail.com>
2016-02-03 00:40:35 +0900

    Tic Game

--- src/atcoder/abc025/QcTicGame.java
+++ src/atcoder/abc025/QcTicGame.java
@@ -1,233 +1,108 @@
 package atcoder.abc025;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.InputMismatchException;
+import java.util.*;
 
 public class QcTicGame {
+    public static int[][] mapB, mapC;
+    public static int pointSum;
+    public static int CHOKUDAI = -1;
+    public static int CHOKUSHO = 1;
+    public static int NOHAND = 0;
+    public static HashMap<Integer, Integer> memo = new HashMap<>();
 
-    static InputStream is;
-    static PrintWriter out;
-    static String INPUT = "";
-
-    final int INF = Integer.MAX_VALUE;
-    final int MIN = Integer.MIN_VALUE;
-
-    static int N;
-    static int M;
-    static int[][] scoreA;
-    static int[][] scoreB;
-    static void solve() {
-        // map = new int[9];
-        N = 3;
-        scoreA = new int[2][3];
-        scoreA = new int[3][2];
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        mapB = new int[2][3];
+        mapC = new int[3][2];
+        pointSum = 0;
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++) {
-                scoreA[i][j] = ni();
+                mapB[i][j] = sc.nextInt();
+                pointSum += mapB[i][j];
             }
         }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 2; j++) {
-                scoreB[i][j] = ni();
+                mapC[i][j] = sc.nextInt();
+                pointSum += mapC[i][j];
             }
         }
 
-        int[] res = score(10);
+        // cmp: chokudai_max_point
+        int cmp = calc(CHOKUDAI, new int[3][3], 0);
+        System.out.println(cmp);
+        System.out.println(pointSum - cmp);
     }
 
-    public static void main(String[] args) throws Exception {
-        long S = System.currentTimeMillis();
-        is = INPUT.isEmpty() ? System.in : new ByteArrayInputStream(INPUT.getBytes());
-        out = new PrintWriter(System.out);
-
-        solve();
-        out.flush();
-        long G = System.currentTimeMillis();
-        tr(G - S + "ms");
-    }
-
-    static int[] score(int d) {
-        if (d == 9) {
-            int ans = 0;
-            for (int i = 0; i < N; i++) {
-            }
-        } else if (d % 2 == 0) {
-            for (int i = 0; i < N; i++) {
-                //for (int j = 0; j < N; j++) {
-                //    if (used[i][j] != 0) {
-                //        continue;
-                //    }
-                //}
-                //used[i][j] = 2;
-                //int tmp = score(d + 1);
-                //ans = Math.max(temp, ans))
-                //used[i][j] = 0;
-            }
+    public static int calc(int hand, int[][] map, int k) {
+        if (k == 9) {
+            return point(map);
         }
-        return new int[3];
-    }
-
-    static int[] calc(int[][] map) {
-        int a = 0;
-        int b = 0;
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (map[i][j] == map[i + 1][j]) {
-                    a += scoreA[i][j];
+        int p = -1;
+        for (int i = 0; i < 9 - k; i++) {
+            int pos = putForEmpty(map, i);
+            int pi = pos / 3;
+            int pj = pos % 3;
+            map[pi][pj] = hand;
+            if (p == -1) {
+                p = calc(-hand, map, k + 1);
+            } else {
+                if (hand == CHOKUDAI) {
+                    p = Math.max(p, calc(-hand, map, k + 1));
                 } else {
-                    b += scoreA[i][j];
+                    p = Math.min(p, calc(-hand, map, k + 1));
                 }
             }
+            map[pi][pj] = NOHAND;
         }
+        return p;
+    }
+
+    /* t + 1番目に空のマスの座標を返す */
+    public static int putForEmpty(int[][] map, int t) {
+        int ec = 0;
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 2; j++) {
-                if (map[i][j] == map[i][j + 1]) {
-                    a += scoreB[i][j];
-                } else {
-                    b += scoreB[i][j];
+            for (int j = 0; j < 3; j++) {
+                if (map[i][j] == NOHAND) {
+                    ec ++;
+                    if (ec == t + 1) {
+                        return i * 3 + j;
+                    }
                 }
             }
-            return new int[] { a, b };
         }
-        return new int[3];
+        return -1;
     }
 
-    private static boolean eof() {
-        if (lenbuf == -1) return true;
-        int lptr = ptrbuf;
-        while (lptr < lenbuf) if (!isSpaceChar(inbuf[lptr++])) return false;
-
-        try {
-            is.mark(1000);
-            while (true) {
-                int b = is.read();
-                if (b == -1) {
-                    is.reset();
-                    return true;
-                } else if (!isSpaceChar(b)) {
-                    is.reset();
-                    return false;
+    public static int point(int[][] map) {
+        int key = toKey(map);
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        int p = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i < 2 && map[i][j] == map[i + 1][j]) {
+                    p += mapB[i][j];
+                }
+                if (j < 2 && map[i][j] == map[i][j + 1]) {
+                    p += mapC[i][j];
                 }
             }
-        } catch (IOException e) {
-            return true;
         }
+        memo.put(key, p);
+        return p;
     }
 
-    private static byte[] inbuf = new byte[1024];
-    static int lenbuf = 0, ptrbuf = 0;
-
-    private static int readByte() {
-        if (lenbuf == -1) throw new InputMismatchException();
-        if (ptrbuf >= lenbuf) {
-            ptrbuf = 0;
-            try {
-                lenbuf = is.read(inbuf);
-            } catch (IOException e) {
-                throw new InputMismatchException();
+    public static int toKey(int[][] map) {
+        int k = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (map[i][j] == CHOKUDAI) {
+                    k |= 1 << (i * 3 + j);
+                }
             }
-            if (lenbuf <= 0) return -1;
         }
-        return inbuf[ptrbuf++];
-    }
-
-    private static boolean isSpaceChar(int c) {
-        return !(c >= 33 && c <= 126);
-    }
-
-    //	private static boolean isSpaceChar(int c) { return !(c >= 32 && c <= 126); }
-    private static int skip() {
-        int b;
-        while ((b = readByte()) != -1 && isSpaceChar(b)) ;
-        return b;
-    }
-
-    private static double nd() {
-        return Double.parseDouble(ns());
-    }
-
-    private static char nc() {
-        return (char) skip();
-    }
-
-    private static String ns() {
-        int b = skip();
-        StringBuilder sb = new StringBuilder();
-        while (!(isSpaceChar(b))) {
-            sb.appendCodePoint(b);
-            b = readByte();
-        }
-        return sb.toString();
-    }
-
-    private static char[] ns(int n) {
-        char[] buf = new char[n];
-        int b = skip(), p = 0;
-        while (p < n && !(isSpaceChar(b))) {
-            buf[p++] = (char) b;
-            b = readByte();
-        }
-        return n == p ? buf : Arrays.copyOf(buf, p);
-    }
-
-    private static char[][] nm(int n, int m) {
-        char[][] map = new char[n][];
-        for (int i = 0; i < n; i++) map[i] = ns(m);
-        return map;
-    }
-
-    private static int[] na(int n) {
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) a[i] = ni();
-        return a;
-    }
-
-    private static int ni() {
-        int num = 0, b;
-        boolean minus = false;
-        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-')) ;
-        if (b == '-') {
-            minus = true;
-            b = readByte();
-        }
-
-        while (true) {
-            if (b >= '0' && b <= '9') {
-                num = num * 10 + (b - '0');
-            } else {
-                return minus ? -num : num;
-            }
-            b = readByte();
-        }
-    }
-
-    private static long nl() {
-        long num = 0;
-        int b;
-        boolean minus = false;
-        while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-')) ;
-        if (b == '-') {
-            minus = true;
-            b = readByte();
-        }
-
-        while (true) {
-            if (b >= '0' && b <= '9') {
-                num = num * 10 + (b - '0');
-            } else {
-                return minus ? -num : num;
-            }
-            b = readByte();
-        }
-    }
-
-    private static void tr(Object... o) {
-        if (INPUT.length() != 0) System.out.println(Arrays.deepToString(o));
+        return k;
     }
 }
-
