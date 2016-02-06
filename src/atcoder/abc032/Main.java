fed941bd6b8b6468576f39848cb300a9a760d798
elzzup <guild0105@gmail.com>
2016-02-06 12:34:34 +0900

    Try abc032

---
+++ src/atcoder/abc032/Main.java
@@ -0,0 +1,34 @@
+package atcoder.abc032;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        Queue<Integer> queue = new ArrayDeque<>();
+        long ss = 1;
+        int nl = 0;
+        for (int i = 0; i < n; i++) {
+            int s = sc.nextInt();
+            if (s == 0) {
+                System.out.println(n);
+                return;
+            }
+            if (s > k) {
+                continue;
+            }
+            while (ss * s > k) {
+                int t = queue.poll();
+                ss /= t;
+            }
+            queue.offer(s);
+            ss *= s;
+            nl = Math.max(queue.size(), nl);
+        }
+        System.out.println(nl);
+    }
+}
