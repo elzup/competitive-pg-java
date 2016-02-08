6a506fa876d04841f4d7c8e1fb870c44c3c4806b
elzzup <guild0105@gmail.com>
2016-02-09 01:33:18 +0900

    Napsack

--- src/atcoder/abc032/Main.java
+++ src/atcoder/abc032/Main.java
@@ -1,34 +1,55 @@
 package atcoder.abc032;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
+    // napsac ナップサック
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int k = sc.nextInt();
-        Queue<Integer> queue = new ArrayDeque<>();
-        long ss = 1;
-        int nl = 0;
+        int capacity = sc.nextInt();
+        Item[] items = new Item[n];
         for (int i = 0; i < n; i++) {
-            int s = sc.nextInt();
-            if (s == 0) {
-                System.out.println(n);
-                return;
-            }
-            if (s > k) {
-                continue;
-            }
-            while (ss * s > k) {
-                int t = queue.poll();
-                ss /= t;
-            }
-            queue.offer(s);
-            ss *= s;
-            nl = Math.max(queue.size(), nl);
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            items[i] = new Item(w, v);
         }
-        System.out.println(nl);
+
+        int[] way = new int[capacity + 1];
+        way[0] = 1;
+        int max = 0;
+        for (Item item : items) {
+            int[] new_way = new int[capacity + 1];
+            for (int i = 0; i < capacity; i++) {
+                if (way[i] != 0) {
+                    new_way[i] = Math.max(new_way[i], way[i]);
+                    max = Math.max(max, new_way[i]);
+                    if (i + item.weight <= capacity) {
+                        new_way[i + item.weight] = way[i] + item.point;
+                        max = Math.max(max, new_way[i + item.weight]);
+                    }
+                }
+            }
+            way = new_way;
+            // arrayDump(way);
+        }
+        System.out.println(max - 1);
+    }
+
+    public static void arrayDump(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%3d", a[i]);
+        }
+        System.out.println();
+    }
+
+    public static class Item {
+        public int weight;
+        public int point;
+
+        public Item(int weight, int point) {
+            this.weight = weight;
+            this.point = point;
+        }
     }
 }
