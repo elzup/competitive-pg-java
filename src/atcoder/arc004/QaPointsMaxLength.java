45b54fa6fbfc695670418812bc09e735f64d78d4
elzzup <guild0105@gmail.com>
2016-02-22 16:14:48 +0900

    Continue arc004

--- src/atcoder/arc004/QaPointsMaxLength.java
+++ src/atcoder/arc004/QaPointsMaxLength.java
@@ -2,7 +2,7 @@ package atcoder.arc004;
 
 import java.util.Scanner;
 
-public class Main {
+public class QaPointsMaxLength {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
