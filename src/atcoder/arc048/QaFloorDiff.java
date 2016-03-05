c68b14891c0e02d7cdd8dfce2fbd5b064b700ecd
elzzup <guild0105@gmail.com>
2016-03-06 03:44:46 +0900

    Try arc048

---
+++ src/atcoder/arc048/QaFloorDiff.java
@@ -0,0 +1,18 @@
+package atcoder.arc048;
+
+import java.util.Scanner;
+
+public class QaFloorDiff {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        if (a < 0) {
+            a ++;
+        }
+        if (b < 0) {
+            b ++;
+        }
+        System.out.println(b - a);
+    }
+}
