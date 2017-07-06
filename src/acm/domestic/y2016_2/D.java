19be2c959bf02b99e8df036ff7b16bd3428c5151
elzup <guild0105@gmail.com>
2017-07-06 10:37:51 +0900

    Update daruma

--- src/acm/domestic/y2016_2/D.java
+++ src/acm/domestic/y2016_2/D.java
@@ -22,7 +22,7 @@ public class D {
           if (w == 2) {
             dp[l][l + 1] = Math.abs(a[l] - a[l + 1]) <= 1;
             any = any || dp[l][l + 1];
-            // System.out.print(dp[l][l + 1] ? "1" : "0");
+//            System.out.printf("%3s", dp[l][l + 1] ? "1" : "0");
             continue;
           }
           int r = l + w;
@@ -34,10 +34,11 @@ public class D {
                     || (dp[l][l + 1] && dp[l + 2][r - 1])
                     || (dp[l + 1][r - 2] && Math.abs(a[l] - a[r - 1]) <= 1);
           }
-          // System.out.print(dp[l][r - 1] ? "1" : "0");
+          // 0, 1, 2, 3, 4, 5, 6, 7
+//          System.out.printf("%3s", dp[l][r - 1] ? "1" : "0");
           any = any || dp[l][r - 1];
         }
-        // System.out.println();
+//        System.out.println();
         if (!any) {
           break;
         }
@@ -45,25 +46,29 @@ public class D {
       int[][] dp2 = new int[n][n];
       for (int i = 0; i < n - 1; i++) {
         dp2[i][i + 1] = dp[i][i + 1] ? 2 : 0;
-        // System.out.print(dp2[i][i + 1]);
+//        System.out.printf("%3d", dp2[i][i + 1]);
       }
-      // System.out.println();
+//      System.out.println();
 
-      for (w = 3; w <= n; w ++) {
+      for (w = 4; w <= n; w += 2) {
         for (int l = 0; l + w - 1 < n; l++) {
           int r = l + w - 1;
           if (dp[l][r]) {
             dp2[l][r] = w;
-            continue;
+          } else {
+            for (int d = l + 2; d <= r - 1; d += 2) {
+              dp2[l][r] = Math.max(dp2[l][r], dp2[l][d - 1] + dp2[d][r]);
+            }
           }
-          for (int d = l + 1; d < r; d++) {
-            dp2[l][r] = Math.max(dp2[l][r], dp2[l][d] + dp2[d + 1][r]);
-          }
-          // System.out.print(dp2[l][r]);
+//          System.out.printf("%3d", dp2[l][r]);
         }
-        // System.out.println();
+//        System.out.println();
       }
-      System.out.println(dp2[0][n - 1]);
+      if (n % 2 == 1) {
+        System.out.println(Math.max(dp2[0][n - 2], dp2[1][n - 1]));
+      } else {
+        System.out.println(dp2[0][n - 1]);
+      }
     }
   }
 }
